27e9f1d814fdc949c37d91e9bbe8b3e614717fe5
Dawid Tyrała <33028235+dominiczeq@users.noreply.github.com>
2019-02-21 23:37:01 +0100

    Added officeList view

--- src/main/java/pl/dejv/carrental/controller/OfficeController.java
+++ src/main/java/pl/dejv/carrental/controller/OfficeController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.dejv.carrental.entity.Office;
 import pl.dejv.carrental.repository.OfficeRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/office")
@@ -26,5 +29,15 @@ public class OfficeController {
     public String addOffice(Office office) {
         officeRepository.save(office);
         return "home";
+    }
+
+    @GetMapping("/list")
+    public String officeList() {
+        return "officeList";
+    }
+
+    @ModelAttribute("allOffices")
+    public List<Office> allOffices() {
+        return this.officeRepository.findAll();
     }
 }
